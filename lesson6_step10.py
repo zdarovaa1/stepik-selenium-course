ed2b0b7380ba1f509f87a6e337caf28defc93c89
aserkov <a.serkov@smartplayer.org>
2023-04-11 22:29:20 +0300

    lesson-6-10 done

---
+++ lesson6_step10.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try:
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    inputs = browser.find_elements(By.TAG_NAME, 'input')
+    data = (
+        'vadim', 'turushev', 'the_best@gmail.com', '+794345322',
+        'lenin street',
+    )
+    index = 0
+    for elem in inputs:
+        elem.send_keys(data[index])
+        index += 1
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(3)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
