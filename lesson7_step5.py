536af907551b71731efd80a58eee83a08f3e73a4
aserkov <a.serkov@smartplayer.org>
2023-04-11 22:29:34 +0300

    lesson-7-5 done

---
+++ lesson7_step5.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+import time
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x = int(x_element.text)
+    y = calc(x)
+
+    input_answer = browser.find_element(By.ID, "answer")
+    input_answer.send_keys(y)
+
+    checkbox = browser.find_element(By.ID, "robotCheckbox")
+    checkbox.click()
+
+    radio_button = browser.find_element(By.ID, "robotsRule")
+    radio_button.click()
+
+    submit_button = browser.find_element(By.TAG_NAME, "button")
+    submit_button.click()
+
+finally:
+    time.sleep(10)
+    browser.quit()
